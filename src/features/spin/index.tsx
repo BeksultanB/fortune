6bf37d29f8175575bdd338dd9e31d9b42a06a69f
Beks <beksbek3@gmail.com>
2024-02-01 02:53:25 +0600

    finished v2.0

--- src/features/spin/index.tsx
+++ src/features/spin/index.tsx
@@ -6,26 +6,37 @@ const Spin = ({ swiperRef, onSpin, slides }: any) => {
     const isSpinning = useRef(false);
 
     const spinHandler = () => {
-        const swiper = swiperRef.current;
         if (!isSpinning.current) {
-            const i = getRandomNumber(0, slides.length - 1);
             isSpinning.current = true;
+            const swiper = swiperRef.current;
+            const duration = getRandomNumber(15, 20) * 1000;
+            const setSpeed = (speed: any, timeout?: any) => {
+                if (timeout) {
+                    setTimeout(() => setSpeed(speed), timeout)
+                } else {
+                    swiper.params.speed = speed;
+                    swiper.update();
+                }
+            }
 
-            swiper.params.speed = 100;
-            swiper.update();
+            let timeout = 5000
+            setSpeed(100)
+
+            for (let speed = 150; timeout < duration; speed += 50) {
+                setSpeed(speed, timeout)
+                timeout = timeout + 500
+            }
 
             setTimeout(() => {
                 swiper.autoplay.stop();
-                swiper.slideToLoop(i);
-
                 setTimeout(async () => {
-                    onSpin?.(() => ({ ...slides[i] }))
+                    onSpin?.(() => ({ ...slides[swiper.realIndex] }))
                     swiper.params.speed = 1000;
                     swiper.update();
-                    !onSpin && swiper.autoplay.start()
+                    !onSpin && swiper.autoplay.start();
                     isSpinning.current = false;
                 }, 700)
-            }, 4000)
+            }, duration)
         }
     }
 
