8120644d9cb825e3e08a323fe0a9186d3d77bbbd
Beks <beksbek3@gmail.com>
2024-02-02 15:26:15 +0600

    Animated congratulations

--- src/entities/fortuneCongratulations/index.tsx
+++ src/entities/fortuneCongratulations/index.tsx
@@ -3,15 +3,39 @@ import s from "./FortuneCongratulations.module.scss"
 import Subtitle from "shared/ui/Subtitle";
 import Text from "shared/ui/Text";
 import FortuneItem from "entities/fortuneItem";
+import { useEffect } from "react";
+import clsx from "clsx";
 
 function FortuneCongratulations({ prize, ...props }: any) {
 
+
+    useEffect(() => {
+        setTimeout(() => {
+            const subtitle = document.querySelector("." + s.subtitle)
+            const you = document.querySelector("." + s.you)
+            const win = document.querySelector("." + s.win)
+            const prize = document.querySelector("." + s.prize)
+
+            //@ts-ignore
+            subtitle && (subtitle.style.left = "0")
+            //@ts-ignore
+            you && (you.style.left = "0");
+            //@ts-ignore
+            win && (win.style.left = "0");
+            //@ts-ignore
+            prize && (prize.style.left = "0");
+        }, 500)
+
+    }, []);
     return (
         prize ? <Congratulations className={s.congratulations} {...props}>
             <div className={s.content}>
                 <div className={s.info}>
                     <Subtitle className={s.subtitle}>Поздравляем!</Subtitle>
-                    <Text className={s.text}>Вы выиграли</Text>
+                    <div className={s.textContainer}>
+                        <Text className={clsx(s.text, s.you)}>Вы</Text>
+                        <Text className={clsx(s.text, s.win)}> выиграли</Text>
+                    </div>
                 </div>
                 <FortuneItem className={s.prize} data={prize} />
             </div>
